f0f0e7b59bee579f97778a8a2c25f0edde0d2e1d
Harsh-Chauhan0000 <harsh.chauhan0000@gmail.com>
2026-08-16 00:17:28 +0530

    train orchestrator

--- src/fminst_pipeline/models/backbone.py
+++ src/fminst_pipeline/models/backbone.py
@@ -2,7 +2,7 @@ from fminst_pipeline.entity.config import Config
 import logging
 
 import torch.nn as nn
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50, ResNet50_Weights, resnet18, ResNet18_Weights, EfficientNet_B0_Weights, efficientnet_b0, VGG16_Weights, vgg16
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +15,36 @@ def create_backbone(config: Config) -> tuple[nn.Module, int]:
             weights = None
 
         model = resnet50(weights=weights)
+        in_features = model.fc.in_features
+        
+        logger.info(f"Created {config.model.model_backbone} backbone with pretrained: {config.model.pretrained}")
+    elif config.model.model_backbone == "resnet18":
+        if config.model.pretrained:
+            weights = ResNet18_Weights.DEFAULT
+        else:
+            weights = None
+
+        model = resnet18(weights=weights)
+        in_features = model.fc.in_features
+        
+        logger.info(f"Created {config.model.model_backbone} backbone with pretrained: {config.model.pretrained}")
+    elif config.model.model_backbone == "efficientsnet_b0":
+        if config.model.pretrained:
+            weights = EfficientNet_B0_Weights.DEFAULT
+        else:
+            weights = None
+
+        model = efficientnet_b0(weights=weights)
+        in_features = model.fc.in_features
+        
+        logger.info(f"Created {config.model.model_backbone} backbone with pretrained: {config.model.pretrained}")
+    elif config.model.model_backbone == "vgg16":
+        if config.model.pretrained:
+            weights = VGG16_Weights.DEFAULT
+        else:
+            weights = None
+
+        model = vgg16(weights=weights)
         in_features = model.fc.in_features
         
         logger.info(f"Created {config.model.model_backbone} backbone with pretrained: {config.model.pretrained}")
--- src/fminst_pipeline/models/classifier.py
+++ src/fminst_pipeline/models/classifier.py
@@ -1,6 +1,7 @@
 import logging
-
+import sys
 from fminst_pipeline.entity.config import Config
+from fminst_pipeline.exception import CustomException
 
 import torch.nn as nn
 
@@ -8,21 +9,22 @@ logger = logging.getLogger(__name__)
 
 def create_classifier(config: Config, backbone_out_features: int) -> nn.Module:
 
-    if config.model.model_backbone == "resnet50":
+    try:
         model = nn.Sequential(
-            nn.Linear(backbone_out_features, 512),
+            nn.Linear(backbone_out_features, backbone_out_features//2),
+            nn.BatchNorm1d(backbone_out_features//2),
             nn.ReLU(),
             nn.Dropout(config.model.dropout),
-            nn.Linear(512, 256),
+            nn.Linear(backbone_out_features//2, backbone_out_features//4),
+            nn.BatchNorm1d(backbone_out_features//4),
             nn.ReLU(),
             nn.Dropout(config.model.dropout),
-            nn.Linear(256, config.model.num_classes)
+            nn.Linear(backbone_out_features//4, config.model.num_classes)
         )
         for param in model.parameters():
             param.requires_grad =True
             
         logger.info(f"Created classifier for backbone: {config.model.model_backbone}")
         return model
-    else:
-        logger.error(f"Unknown backbone: {config.model.model_backbone}")
-        raise ValueError(f"Unknown backbone: {config.model.model_backbone}")
+    except Exception as e:
+        raise CustomException(f"Failed to create classifier - {str(e)}", sys)
